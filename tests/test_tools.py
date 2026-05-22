73641e4b366b4817ba18b48ad3b91f14dd6919ea
Ryan <ryan@ispeakcomputer.com>
2026-05-22 15:59:43 -0600

    test: remove internal IP from test config

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -8,7 +8,7 @@ from unittest.mock import patch, MagicMock
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ["OPENAI_API_BASE"] = "http://192.168.1.33:8080/v1"
+os.environ["OPENAI_API_BASE"] = os.environ.get("OPENAI_API_BASE", "http://localhost:8080/v1")
 os.environ["OPENAI_API_KEY"] = "test-key"
 os.environ["MODEL_NAME"] = "test-model"
 
